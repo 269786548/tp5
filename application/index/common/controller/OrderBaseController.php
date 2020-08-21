7539d50eac8a6a1be19676b7da0d0ecb41eedc8a
269786548@qq.com <269786548@qq.com>
2020-08-21 11:01:13 +0800

    test

--- application/index/common/controller/OrderBaseController.php
+++ application/index/common/controller/OrderBaseController.php
@@ -13,16 +13,16 @@ class OrderBaseController extends Controller
 		$postJson = file_get_contents('php://input');//得到参数
 		Log::info($postJson);
 		$this->postJson = json_decode($postJson,true);
-		if(empty($this->postJson)){
-			$return_data['code'] = 400;
-			$return_data['message'] = 'need parameters!!!';
-			return json($return_data);
-		}
 
 	}
 
 
 	public function getService($serviceMethod){
+		if(empty($this->postJson)){
+			$return_data['code'] = 400;
+			$return_data['message'] = 'need parameters!!!';
+			return json($return_data);
+		}
 		try
 		{
 			$orderService = new OrderService($this->postJson);
